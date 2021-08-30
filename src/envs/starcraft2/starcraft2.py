773b3a30693c6ec76b941698f075c88ba8ae250d
Xu Zhiwei <1027660817@qq.com>
2021-08-30 22:20:01 +0800

    Update starcraft2.py

--- src/envs/starcraft2/starcraft2.py
+++ src/envs/starcraft2/starcraft2.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from smac.env.multiagentenv import MultiAgentEnv
+from envs.multiagentenv import MultiAgentEnv
 from smac.env.starcraft2.maps import get_map_params
 
 import atexit
